58fda794616b91f1ba56715d23a537dddec0e7a5
Aly BA <aly-fr@outlook.fr>
2020-12-11 12:47:34 +0100

    ok

---
+++ commande.ps1
@@ -0,0 +1,12 @@
+﻿git version 
+-----------
+git init
+---------------
+git clone rep
+--------------
+git diff
+---------------
+git commit --amend
+-------------------
+git mv file destination/(un simple git status)
+-----------------------
